7212ba457553f78ccf7eb42d83ab9d851c8d13fc
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-03-13 12:29:40 +0100

    Add cloufront_hosted_zone_id output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "cloudfront_url" {
 output "policy_arn" {
     value = "${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"
 }
+
+output "cloufront_hosted_zone_id" {
+    value = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+}
