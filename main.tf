cd7e478358ce3faf7f70896d4f8b251bcb96e2ba
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-03-13 17:07:49 +0100

    Remove older TLS protocol

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       http_port = 80
       https_port = 443
       origin_protocol_policy = "https-only"
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_ssl_protocols = ["TLSv1.2"]
     }
   }
 
